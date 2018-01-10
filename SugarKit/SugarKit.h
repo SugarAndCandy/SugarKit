50fb0f7cf8d966dfdf5b7ba30e66b141573d92af
MAXIM KOLESNIK <makleso6@gmail.com>
2018-01-11 02:49:18 +0300

    initial

---
+++ SugarKit/SugarKit.h
@@ -0,0 +1,19 @@
+//
+//  SugarKit.h
+//  SugarKit
+//
+//  Created by MAXIM KOLESNIK on 11.01.2018.
+//  Copyright © 2018 Sugar and Candy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SugarKit.
+FOUNDATION_EXPORT double SugarKitVersionNumber;
+
+//! Project version string for SugarKit.
+FOUNDATION_EXPORT const unsigned char SugarKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SugarKit/PublicHeader.h>
+
+
---
+++ SugarKit/SugarKit.swift
@@ -0,0 +1,13 @@
+//
+//  SugarKit.swift
+//  SugarKit
+//
+//  Created by MAXIM KOLESNIK on 11.01.2018.
+//  Copyright © 2018 Sugar and Candy. All rights reserved.
+//
+
+import Foundation
+
+class SugarKit {
+    
+}
